40da4a79cb6dd16e31d815244fa6c816bae5fb5d
Victor Lowther <victor.lowther@gmail.com>
2015-02-24 14:57:19 -0600

    Fix typo in comment for SOAP namespaces

--- namespaces.go
+++ namespaces.go
@@ -1,6 +1,6 @@
 package soap
 
-// Default SOAP namespaces.  Taked from appendix 1 of
+// Default SOAP namespaces.  Taken from appendix 1 of
 // http://www.dmtf.org/sites/default/files/standards/documents/DSP0226_1.2.0.pdf
 const NS_ENVELOPE = "http://www.w3.org/2003/05/soap-envelope"
 const NS_ENCODING = "http://www.w3.org/2003/05/soap-encoding"
