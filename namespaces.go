5a8b5944c611c4e0dc6ff846d6eda29f457e185a
Victor Lowther <victor.lowther@gmail.com>
2015-02-24 14:32:04 -0600

    Change namespaces to constants

--- namespaces.go
+++ namespaces.go
@@ -2,7 +2,5 @@ package soap
 
 // Default SOAP namespaces.  Taked from appendix 1 of
 // http://www.dmtf.org/sites/default/files/standards/documents/DSP0226_1.2.0.pdf
-var (
-	NS_ENVELOPE = "http://www.w3.org/2003/05/soap-envelope"
-	NS_ENCODING = "http://www.w3.org/2003/05/soap-encoding"
-)
+const NS_ENVELOPE = "http://www.w3.org/2003/05/soap-envelope"
+const NS_ENCODING = "http://www.w3.org/2003/05/soap-encoding"
