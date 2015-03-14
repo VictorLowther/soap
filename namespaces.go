8e36fca84b22ee6551be6621e345aaf2fa9c6c83
Victor Lowther <victor.lowther@gmail.com>
2015-03-14 10:15:24 -0500

    Add license headers to files

--- namespaces.go
+++ namespaces.go
@@ -1,5 +1,21 @@
 package soap
 
+/*
+Copyright 2015 Victor Lowther <victor.lowther@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // Default SOAP namespaces.  Taken from appendix 1 of
 // http://www.dmtf.org/sites/default/files/standards/documents/DSP0226_1.2.0.pdf
 const NS_ENVELOPE = "http://www.w3.org/2003/05/soap-envelope"
